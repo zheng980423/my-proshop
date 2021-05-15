9a82d63709a2c6e2030cf9efcd3a5dceb65ef465
zwf <zwf19980423@gmail.com>
2021-05-15 21:56:22 +0800

    create react redux

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+
 import asyncHandler from 'express-async-handler';
 const router = express.Router();
 import Product from '../models/productModel.js';
@@ -24,7 +25,7 @@ router.get(
       res.json(product);
     } else {
       res.status(404);
-      throw new Error('product not found');
+      throw new Error('Product not found');
     }
   })
 );
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 import connectDB from './config/db.js';
 import colors from 'colors';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
@@ -16,6 +16,9 @@ app.get('/', (req, res) => {
   res.send('API is running....');
 });
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
